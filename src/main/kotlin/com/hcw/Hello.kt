c6395a7adf01412c8ad472bf68b0adc70b566951
Wayne Hung <thunder.wayne@gmail.com>
2019-05-08 16:21:38 +0800

    Initial commit

---
+++ src/main/kotlin/com/hcw/Hello.kt
@@ -0,0 +1,14 @@
+package com.hcw
+
+fun main() {
+//    println("Hello Kotlin!")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human {
+    fun hello(){
+        println("Hello Kotlin")
+    }
+}
